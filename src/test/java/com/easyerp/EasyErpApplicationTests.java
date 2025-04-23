58359bb821e40766e7cd15d9777c7a8d04372449
messiasfernandes <messiasfernandes@gmail.com>
2025-04-23 17:57:12 -0300

    deletando arquivo desnecessarios

--- src/test/java/com/easyerp/EasyErpApplicationTests.java
+++ src/test/java/com/easyerp/EasyErpApplicationTests.java
@@ -2,7 +2,6 @@ package com.easyerp;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
